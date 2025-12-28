4acf7e1c14a48186a9ef147b184488671f6529f0
Rohani Game Dev <fortolotos4@gmail.com>
2025-12-28 15:38:33 +0700

    bug tampilan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,19 +13,21 @@
   </style>
 </head>
 
-<body class="bg-gray-100" x-cloak>
+<body class="bg-gray-100">
 
 <div x-data="{ sidebarOpen: false }" class="flex min-h-screen">
 
   <!-- OVERLAY MOBILE -->
   <div
     x-show="sidebarOpen"
+    x-cloak
     @click="sidebarOpen=false"
     class="fixed inset-0 bg-black/40 z-30 md:hidden">
   </div>
 
   <!-- SIDEBAR -->
   <aside
+    x-cloak
     class="fixed md:static inset-y-0 left-0 z-40 w-64 bg-white border-r
            transform transition-transform duration-200
            md:translate-x-0"
@@ -35,11 +37,17 @@
       🎮 Game Rohani
     </div>
 
-    @yield('sidebar')
+    @hasSection('sidebar')
+      @yield('sidebar')
+    @else
+      <nav class="px-4 text-sm text-gray-400">
+        Menu
+      </nav>
+    @endif
   </aside>
 
   <!-- CONTENT -->
-  <main class="flex-1 md:ml-64">
+  <main class="flex-1 min-h-screen">
 
     <!-- HEADER MOBILE -->
     <div class="md:hidden flex items-center justify-between p-4 bg-white shadow">

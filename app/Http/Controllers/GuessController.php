52a58691d8bfcf08f7a095a6b27eab010b90f1a3
Rohani Game Dev <fortolotos4@gmail.com>
2025-12-30 14:48:53 +0700

    Final

--- app/Http/Controllers/GuessController.php
+++ app/Http/Controllers/GuessController.php
@@ -26,7 +26,7 @@ class GuessController extends Controller
                 'image_path' => $q->image_path,
                 'answer_text' => $q->answer_text,
                 'answer_slots' => $q->answer_slots,
-                'time_limit_seconds' => $q->time_limit_seconds ?? 16,
+                'time_limit_seconds' => $q->time_limit_seconds ?? 45,
             ];
         })
         ->toArray();
